b8e3097b1342ea372c4d068f1acf70d7d8a6da38
Fwamba <andrewecorrex03@gmail.com>
2023-02-27 12:36:06 +0300

    task:Side bar items

---
+++ src/components/SideBarData.js
@@ -0,0 +1,43 @@
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+export const SideBarData = [
+  {
+    title: "Home",
+    path: "/",
+    icons: <AiIcons.AiFillHome />,
+    cName: "nav-text",
+  },
+  {
+    title: "Reports",
+    path: "/reports",
+    icons: <IoIcons.IoIosPaper />,
+    cName: "nav-text",
+  },
+  {
+    title: "Products",
+    path: "/products",
+    icons: <FaIcons.FaCartPlus />,
+    cName: "nav-text",
+  },
+  {
+    title: "Team",
+    path: "/team",
+    icons: <IoIcons.IoMdPeople />,
+    cName: "nav-text",
+  },
+  {
+    title: "Messages",
+    path: "/messages",
+    icons: <FaIcons.FaEnvelopeOpenText />,
+    cName: "nav-text",
+  },
+  {
+    title: "Support",
+    path: "/support",
+    icons: <IoIcons.IoMdHelpCircle />,
+    cName: "nav-text",
+  },
+];
