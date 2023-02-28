1e62353e12b7b721cd5d8796cee8aa1cdede5f5c
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-03-01 02:55:45 +0300

    task:outlet error

--- src/index.js
+++ src/index.js
@@ -2,34 +2,36 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
-import Home from "./pages/Home";
-import Products from "./pages/Products";
-import './App.css';
-import ErrorPage from "./pages/error-page";
+import Home from "./routes/Home";
+import Products from "./routes/Products";
+import "./App.css";
+import ErrorPage from "./routes/error-page";
+import Empty from "./routes/default";
 
 const AppLayout = () => (
   <>
     <Sidebar />
-    <Outlet/>
+    <Outlet />
   </>
 );
 
 const router = createBrowserRouter([
   {
-    element: <AppLayout/>,
-    errorElement: <ErrorPage/>,
-    children:[
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: false, element: <Empty /> },
       {
         path: "/",
+        errorElement: <ErrorPage />,
         element: <Home />,
       },
       {
         path: "products",
         element: <Products />,
       },
-    ]
+    ],
   },
-
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
