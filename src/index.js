7304dd122e2712356f07813d4fc2ae3513c9a510
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-03-04 23:47:47 +0300

    task: import error fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Home from "./routes/Home";
@@ -45,6 +45,6 @@ const router = createBrowserRouter([
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
 );
