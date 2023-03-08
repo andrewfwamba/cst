8c4da81ea6c8ac8fe9292cba79e2b532a3f7c1b2
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-03-08 17:32:12 +0300

    task: added urls to backend

---
+++ src/utilities/client.js
@@ -0,0 +1,8 @@
+// calls to api
+
+const baseUrl = "https://83e4-41-90-71-43.in.ngrok.io/api/v1";
+
+export const createUser = `${baseUrl}/create-user`;
+export const updateUser = `${baseUrl}/users/update`;
+export const deleteUser = `${baseUrl}/users/delete`;
+export const loginUser = `${baseUrl}/users/login`;
