2fa15801e8296d3939bfbe2b51ffb1ca7e8ffb3b
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-03-15 23:46:01 +0300

    task: minor fixes

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -17,8 +17,8 @@ function Home() {
     return <Loader />;
   } else
     return (
-      <div className="container">
-        <div className="landing-wrapper mt-2 radius mb-2">
+      <div className="mx-auto content-center">
+        <div className="landing-wrapper mt-2 radius mb-2 md:max-w-max">
           <div className="landing-1">
             <h3>Get Clarity for Your Organization's Performance</h3>
             <p>
@@ -34,6 +34,20 @@ function Home() {
         </div>
         <hr />
         <div className="info"></div>
+        <header className="bg-white shadow">
+          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
+          </div>
+        </header>
+        <main>
+          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+            {/* <!-- Replace with your content --> */}
+            <div className="px-4 py-6 sm:px-0">
+              {/* <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div> */}
+            </div>
+            {/* <!-- /End replace --> */}
+          </div>
+        </main>
       </div>
     );
 }
