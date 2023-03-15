e21e0df2642a70d050e29f988dc3f41c46cfc2a6
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-03-15 23:48:38 +0300

    task: added grap chart

--- src/routes/Stats.js
+++ src/routes/Stats.js
@@ -1,21 +1,8 @@
 import React from "react";
-import { Bar } from "react-chartjs-2";
+import Bargraph from "../utilities/Bargraph";
+//
 
 function Stats() {
-  const data = {
-    labels: ["January", "Februrary", "March", "April", "May", "June", "July"],
-    datasets: [
-      {
-        label: "Test data",
-        backgroundColor: "rgba(255, 99, 132, 0.2)",
-        borderColor: "rgba(255, 99, 132, 1)",
-        borderWidth: 1,
-        hoverBackgroundColor: "rgba(255,99,132,0.4)",
-        hoverBorderColor: "rgba(255, 99, 132, 1)",
-        data: [65, 59, 80, 81, 56, 55, 40],
-      },
-    ],
-  };
   return (
     <div className="container">
       <div className="">
@@ -36,15 +23,7 @@ function Stats() {
           </div>
         </div>
         <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-          {/* <Bar
-          data={data}
-          width={100}
-          height={50}
-          option={{
-            maintainAspectRatio: false,
-          }}
-        /> */}
-          <p>This a test for display flex.</p>
+          <Bargraph />
         </div>
       </div>
     </div>
