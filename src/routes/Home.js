40ecda0e80e79f2d0c0d5158f420961d843197ad
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-03-16 02:58:18 +0300

    task: design update

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -18,6 +18,18 @@ function Home() {
   } else
     return (
       <div className="mx-auto content-center">
+        <header className="bg-white shadow">
+          <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+              Dashboard
+            </h1>
+          </div>
+        </header>
+        <main>
+          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            {/* Your content */}
+          </div>
+        </main>
         <div className="landing-wrapper mt-2 radius mb-2 md:max-w-max">
           <div className="landing-1">
             <h3>Get Clarity for Your Organization's Performance</h3>
@@ -34,20 +46,6 @@ function Home() {
         </div>
         <hr />
         <div className="info"></div>
-        <header className="bg-white shadow">
-          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
-          </div>
-        </header>
-        <main>
-          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-            {/* <!-- Replace with your content --> */}
-            <div className="px-4 py-6 sm:px-0">
-              {/* <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div> */}
-            </div>
-            {/* <!-- /End replace --> */}
-          </div>
-        </main>
       </div>
     );
 }
