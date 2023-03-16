5aea8f2a27ee642e7afe63dba99b39bace3190b1
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-03-16 03:01:02 +0300

    task: added static data

--- src/utilities/Bargraph.js
+++ src/utilities/Bargraph.js
@@ -20,16 +20,29 @@ Chart.register(
 
 function Bargraph() {
   const data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    labels: [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ],
     datasets: [
       {
         label: "Test data",
-        backgroundColor: "rgba(255, 99, 132, 0.2)",
-        borderColor: "ring ring-1 ring-amber-600",
+        backgroundColor: "rgba(180, 83, 9, 0.2)",
+        borderColor: "rgba(180, 83, 9, 1)",
         borderWidth: 1,
-        hoverBackgroundColor: "rgba(255,99,132,0.4)",
-        hoverBorderColor: "rgba(255, 99, 132, 1)",
-        data: [65, 59, 80, 81, 56, 55, 40],
+        hoverBackgroundColor: "rgba(180, 83, 9,0.4)",
+        hoverBorderColor: "rgba(180, 83, 9, 1)",
+        data: [65, 59, 80, 81, 56, 55, 40, 70, 200, 150, 105, 98],
       },
     ],
   };
@@ -39,7 +52,7 @@ function Bargraph() {
       width={700}
       height={400}
       option={{
-        maintainAspectRatio: true,
+        maintainAspectRatio: false,
       }}
     />
   );
