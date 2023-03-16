b8a20b0190b4582307b16b51c7c2f83fc672aab6
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-03-17 00:23:37 +0300

    task: more components

--- src/routes/Stats.js
+++ src/routes/Stats.js
@@ -1,15 +1,16 @@
 import React from "react";
+import Marketing from "../components/Marketing";
 import Bargraph from "../utilities/Bargraph";
 //
 
 function Stats() {
   return (
-    <div className="container">
-      <div className="">
+    <div className="flex flex-col">
+      <div className="flex flex-initial mx-8 my-4">
         <p className="text-2xl font-bold">Statistics</p>
       </div>
 
-      <div className="inline-flex space-x-8 space-y-2 w-auto">
+      <div className="mx-auto inline-flex space-x-8 space-y-2 w-auto">
         <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
           <div>
             <div class="text-sm font-medium text-black">
@@ -21,6 +22,9 @@ function Stats() {
         <div className="p-6 max-w-xl ring-1 ring-amber-300 mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
           <Bargraph />
         </div>
+      </div>
+      <div>
+        <Marketing />
       </div>
     </div>
   );
