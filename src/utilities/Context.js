e2416f38ff3cc3cbdc38c717facc657666483364
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-03-21 22:11:55 +0300

    task: configured userlogin context

--- src/utilities/Context.js
+++ src/utilities/Context.js
@@ -1,3 +1,43 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const userContext = createContext(null);
+
+const LoginProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  const [loginPending, setLoginPending] = useState(false);
+
+  const fetchUser = async () => {
+    setLoginPending(true);
+    const userData = await localStorage.getItem("user");
+    let user = JSON.parse(userData);
+    setUser(user);
+    console.log(user);
+    if (userData !== null) {
+      setLoggedIn(true);
+      setLoginPending(false);
+    } else {
+      setLoggedIn(false);
+      setLoginPending(false);
+    }
+  };
+  useEffect(() => {
+    fetchUser();
+  }, []);
+  return (
+    <userContext.Provider
+      value={{
+        loggedIn,
+        setLoggedIn,
+        user,
+        setUser,
+        loginPending,
+        setLoginPending,
+      }}
+    >
+      {children}
+    </userContext.Provider>
+  );
+};
+export const useLogin = () => userContext(userContext);
+export default LoginProvider;
