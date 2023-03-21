f92d4eea6aab0593c6a3d8553d305d5c29d2989d
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-03-21 22:11:25 +0300

    task: added footer

--- src/routes/default.jsx
+++ src/routes/default.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import Footer from "../components/Footer";
 
 function Empty() {
   return (
-    <div className="container anime">
-      <svg viewBox="0 0 450 50">
-        <text className="show-case" y="50">
-          Items will appear here
-        </text>
-      </svg>
-    </div>
+    <>
+      <div className="container anime">
+        <svg viewBox="0 0 450 50">
+          <text className="show-case" y="50">
+            Items will appear here
+          </text>
+        </svg>
+      </div>
+      <Footer />
+    </>
   );
 }
 
