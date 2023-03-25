e96bb0253bbfb4e427e4eea7e26fe521cc570d69
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-03-25 22:30:45 +0300

    task: design refix

--- src/routes/Stats.js
+++ src/routes/Stats.js
@@ -1,32 +1,28 @@
 import React from "react";
 import Marketing from "../components/Marketing";
 import Bargraph from "../utilities/Bargraph";
+import Footer from "../components/Footer";
 //
 
 function Stats() {
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-initial mx-8 my-4">
-        <p className="text-2xl font-bold">Statistics</p>
-      </div>
+    <>
+      <div className="flex flex-col">
+        <div className="flex flex-initial mx-8 my-4">
+          <p className="text-2xl font-bold">Statistics</p>
+        </div>
 
-      <div className="mx-auto inline-flex space-x-8 space-y-2 w-auto">
-        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-          <div>
-            <div class="text-sm font-medium text-black">
-              Walk your journey with us
-            </div>
-            <p class="text-slate-500">Always at your service</p>
+        <div className="mx-auto inline-flex space-x-8 space-y-2 w-auto">
+          <div className="p-4 max-w-xl ring-1 md:flex ring-amber-300 mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+            <Bargraph />
           </div>
         </div>
-        <div className="p-6 max-w-xl ring-1 ring-amber-300 mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-          <Bargraph />
+        <div>
+          <Marketing />
         </div>
       </div>
-      <div>
-        <Marketing />
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
