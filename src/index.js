122b022432eeeb4bbb1f1f90d7033598cd23438e
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-04-01 15:09:07 +0300

    fix: cleanup

--- src/index.js
+++ src/index.js
@@ -13,10 +13,7 @@ import Products from "./routes/Products";
 import "./App.css";
 import "./Utils.css";
 import ErrorPage from "./routes/error-page";
-import Empty from "./routes/default";
-import Login from "./routes/Login";
 import Stats from "./routes/Stats";
-import Signup from "./routes/Signup";
 import Support from "./routes/Support";
 
 const AppLayout = () => (
@@ -27,54 +24,13 @@ const AppLayout = () => (
 );
 
 const router = createBrowserRouter(
-  // [
-  // {
-  //   path: "login",
-  //   element: <Login />,
-  //   errorElement: <ErrorPage />,
-  // },
-  // {
-  //   path: "signup",
-  //   element: <Signup />,
-  //   errorElement: <ErrorPage />,
-  // },
-  // {
-  //   path: "/",
-  //   element: <AppLayout />,
-  //   errorElement: <ErrorPage />,
-  //   children: [
-  //     {
-  //       errorElement: <ErrorPage />,
-  //       children: [
-  //         { index: true, element: <Empty /> },
-  //         {
-  //           path: "home",
-  //           element: <Home />,
-  //         },
-  //         {
-  //           path: "statistics",
-  //           element: <Stats />,
-  //         },
-  //         {
-  //           path: "support",
-  //           element: <Support />,
-  //         },
-  //         {
-  //           path: "products",
-  //           element: <Products />,
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // },
-  // ]
   createRoutesFromElements(
-    <Route path="/" element={<AppLayout />} errorElement={<ErrorPage />}>
+    <Route element={<AppLayout />} errorElement={<ErrorPage />}>
       <Route errorElement={<ErrorPage />}>
         <Route index element={<Home />} />
-        <Route path="/statistics" element={<Stats />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/support" element={<Support />} />
+        <Route exact path="/statistics" element={<Stats />} />
+        <Route exact path="/products" element={<Products />} />
+        <Route exact path="/support" element={<Support />} />
         <Route />
       </Route>
     </Route>
