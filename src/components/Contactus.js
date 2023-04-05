42322d90c79c1a2c3eb1aa9ad88c744e53cf987d
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-04-05 22:35:10 +0300

    add: phone and mail

--- src/components/Contactus.js
+++ src/components/Contactus.js
@@ -61,7 +61,7 @@ export default function Contactus() {
   };
 
   return (
-    <div className="animate__animated animate__fadeInTopRight isolate bg-white py-5 px-6 sm:py-4 lg:px-8">
+    <div className="animate__animated animate__fadeInTopRight isolate bg-gray-100 py-5 px-6 sm:py-4 lg:px-8">
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           Talk to us
