64e35fa4d8882593da349e2695dd65fa22bbf41f
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-04-05 22:31:20 +0300

    fix: synchronize theme

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -31,7 +31,8 @@ export default function Accordion() {
               </Disclosure.Button>
               <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
                 If you're unhappy with your purchase for any reason, email us
-                within 90 days and we'll refund you in full, no questions asked.
+                within the first 90 days and we'll refund you in full, no
+                questions asked.
               </Disclosure.Panel>
             </>
           )}
