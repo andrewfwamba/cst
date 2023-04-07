9c5edb12670242f9e8cfefa89771858d38de4ce6
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-04-07 21:40:44 +0300

    updated graph type

--- src/routes/Stats.js
+++ src/routes/Stats.js
@@ -13,8 +13,24 @@ function Stats() {
         </div>
 
         <div className="mx-auto inline-flex space-x-8 space-y-2 w-auto">
-          <div className="p-4 max-w-xl ring-1 md:flex ring-amber-300 mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-            <Bargraph />
+          <div className="p-8 grid w-full lg:grid-cols-2 gap-2 ">
+            <div className="ring-gray-400 bg-white ring-1 p-2 lg:p-6 shadow-lg mx-auto hover:shadow-sm w-11/12 lg:w-full hover:cursor-pointer rounded-sm ">
+              <h4 className="text-gray-600 text-xl font-bold">Clients</h4>
+              <Bargraph />
+            </div>
+            <div className="bg-gray-200 rounded-sm shadow-xl hover:shadow-inner mx-auto p-6 lg:p-10">
+              <h4 className="text-center text-xl font-bold text-gray-700 ">
+                Satisfied clients are what we care about.
+              </h4>
+              <p className="text-blue-900 text-semibold text-lg">
+                Join our community and get the best software solutions for your
+                business
+              </p>
+              <p className="text-blue-900 text-semibold text-lg">
+                Enterprise level software solutions that scale with your level
+                of operation.
+              </p>
+            </div>
           </div>
         </div>
         <div>
