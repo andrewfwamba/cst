529a0b17e06fc8ded8493e87fe93b783061e8d09
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-04-07 21:41:12 +0300

    changed graph type

--- src/utilities/Bargraph.js
+++ src/utilities/Bargraph.js
@@ -8,7 +8,7 @@ import {
   PointElement,
   BarElement,
 } from "chart.js";
-import { Bar } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 Chart.register(
   CategoryScale,
   LineElement,
@@ -19,42 +19,62 @@ Chart.register(
 );
 
 function Bargraph() {
-  const data = {
-    labels: [
-      "Jan",
-      "Feb",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "Aug",
-      "Sept",
-      "Oct",
-      "Nov",
-      "Dec",
-    ],
-    datasets: [
-      {
-        label: "Test data",
-        backgroundColor: "rgba(180, 83, 9, 0.2)",
-        borderColor: "rgba(180, 83, 9, 1)",
-        borderWidth: 1,
-        hoverBackgroundColor: "rgba(180, 83, 9,0.4)",
-        hoverBorderColor: "rgba(180, 83, 9, 1)",
-        data: [65, 59, 80, 81, 56, 55, 40, 70, 200, 150, 105, 98],
-      },
-    ],
-  };
+  // const data = {
+  //   labels: [
+  //     "Jan",
+  //     "Feb",
+  //     "March",
+  //     "April",
+  //     "May",
+  //     "June",
+  //     "July",
+  //     "Aug",
+  //     "Sept",
+  //     "Oct",
+  //     "Nov",
+  //     "Dec",
+  //   ],
+  //   datasets: [
+  //     {
+  //       label: "Test data",
+  //       backgroundColor: "rgba(180, 83, 9, 0.2)",
+  //       borderColor: "rgba(180, 83, 9, 1)",
+  //       borderWidth: 1,
+  //       hoverBackgroundColor: "rgba(180, 83, 9,0.4)",
+  //       hoverBorderColor: "rgba(180, 83, 9, 1)",
+  //       data: [65, 59, 80, 81, 56, 55, 40, 70, 200, 150, 105, 98],
+  //     },
+  //   ],
+  // };
   return (
-    <Bar
-      data={data}
-      width={400}
-      height={200}
-      option={{
-        maintainAspectRatio: false,
-      }}
-    />
+    // <Bar
+    //   data={data}
+    //   width={400}
+    //   height={200}
+    //   option={{
+    //     maintainAspectRatio: false,
+    //   }}
+    // />
+    <div className="h-full w-full p-4">
+      <Line
+        data={{
+          // x-axis label values
+          labels: ["1", "2", "3", "4", "5", "6", "7"],
+          datasets: [
+            {
+              label: "# Clients",
+              // y-axis data plotting values
+              data: [200, 300, 500, 1020, 1500, 2350, 3150],
+              fill: false,
+              borderWidth: 4,
+              backgroundColor: "blue",
+              borderColor: "lightgreen",
+              responsive: true,
+            },
+          ],
+        }}
+      />
+    </div>
   );
 }
 
