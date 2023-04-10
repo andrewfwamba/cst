557302385c8187c5727938080d6ff4465b4d6989
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-04-10 23:21:34 +0300

    created about route page

---
+++ src/routes/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="text-gray-700 font-semibold text-center mt-36">
+      <p>Under Construction</p>
+    </div>
+  );
+}
+
+export default About;
