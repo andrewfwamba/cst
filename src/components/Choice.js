003f0193f2b58e0cb2083d9acc91b0f7cc64593e
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-04-10 23:51:43 +0300

    bug fix

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -6,7 +6,7 @@ function Choice() {
     <>
       <div className="grid lg:grid-cols-2 gap-2 p-4">
         {/* Choices 1st */}
-        <div className="bg-gray-100 rounded p-4 lg:mx-0 ">
+        <div className="bg-gray-100 rounded p-2 lg:p-4 lg:mx-0 ">
           <h4 className="text-center sm:text-left px-10 text-base font-bold text-red-500">
             Why Choose us
           </h4>
@@ -16,7 +16,7 @@ function Choice() {
             Company?
           </p>
           {/* Mobile dev portion */}
-          <div className="bg-gray-300  rounded p-4 shadow mb-4 shadow-blue-400 ">
+          <div className="bg-gray-300  rounded p-2 lg:p-4 shadow mb-4 shadow-blue-400 ">
             <div className="flex">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -24,7 +24,7 @@ function Choice() {
                 viewBox="0 0 24 24"
                 stroke-width="1.5"
                 stroke="currentColor"
-                class="w-8 h-8 text-blue-800 mr-8"
+                class="w-8 h-8 text-blue-800 lg:mr-1"
               >
                 <path
                   stroke-linecap="round"
@@ -32,29 +32,29 @@ function Choice() {
                   d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25"
                 />
               </svg>
-              <h2 className="text-2xl text-gray-500 ">
+              <h2 className="text-lg ml-2 lg:text-2xl font-semibold text-gray-500 lg:ml-4">
                 Mobile Apps Development
               </h2>
             </div>
 
-            <div className="px-11">
-              <p className="ml-4 text-gray-600 text-base">
+            <div className="px-3 lg:px-11">
+              <p className="lg:ml-4 text-gray-600 text-base">
                 For both personal and professional usage, we provide platforms
                 for Windows, Android, and mobile web.
               </p>
             </div>
           </div>
           {/* Web dev illustration */}
-          <div className="bg-gray-300  rounded p-4 shadow shadow-blue-400 mb-4">
+          <div className="bg-gray-300  rounded p-2 lg:p-4 shadow shadow-blue-400 mb-4">
             <div className="flex">
               <FcKindle size={30} />
-              <h2 className="text-2xl text-gray-500 ml-4">
+              <h2 className="text-base ml-2 lg:text-2xl font-semibold text-gray-500 lg:ml-4">
                 Web Applications Development
               </h2>
             </div>
 
-            <div className="px-11">
-              <p className="ml-4 text-gray-600 text-base">
+            <div className="px-3 lg:px-11">
+              <p className="lg:ml-4 text-gray-600 text-base">
                 (UXD, UED, or XD) is the process of raising user satisfaction by
                 boosting the product's usability, accessibility, and enjoyable
                 user experience.
@@ -62,16 +62,16 @@ function Choice() {
             </div>
           </div>
           {/* UI/UX */}
-          <div className="bg-gray-300  rounded p-4 shadow shadow-blue-400 ">
+          <div className="bg-gray-300  rounded p-2 lg:p-4 shadow shadow-blue-400 ">
             <div className="flex">
-              <FcMultipleSmartphones size={35} />
-              <h2 className="text-2xl text-gray-500 ml-4">
+              <FcMultipleSmartphones size={30} />
+              <h2 className="text-lg ml-2 lg:text-2xl font-semibold text-gray-500 lg:ml-4">
                 User experience design
               </h2>
             </div>
 
-            <div className="px-11">
-              <p className="ml-4 text-gray-600 text-base">
+            <div className="px-3 lg:px-11">
+              <p className="lg:ml-4 text-gray-600 text-base">
                 We create web pages and applications that are user-friendly,
                 versatile, compliant with enterprise standards, Google search
                 engine friendly, and include analytics to track the
