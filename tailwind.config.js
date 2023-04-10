08c4ec02c77ee1613a25f3e0f0be2d6921289473
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-04-10 23:18:37 +0300

    added new configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    patterns: {
+      opacities: {
+        100: "1",
+        80: ".80",
+        60: ".60",
+        40: ".40",
+        20: ".20",
+        10: ".10",
+        5: ".05",
+      },
+      sizes: {
+        1: "0.25rem",
+        2: "0.5rem",
+        4: "1rem",
+        6: "1.5rem",
+        8: "2rem",
+        16: "4rem",
+        20: "5rem",
+        24: "6rem",
+        32: "8rem",
+      },
+    },
     extend: {},
   },
   plugins: [
@@ -9,5 +31,6 @@ module.exports = {
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/forms"),
     require("@material-tailwind/react"),
+    require("tailwindcss-bg-patterns"),
   ],
 };
