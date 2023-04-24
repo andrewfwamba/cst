764e801d8de9b0efe0e3a1e045d755f9bab2c350
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-04-24 23:14:02 +0300

    populate about

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,29 +1,31 @@
 import React from "react";
+import Team from "./Team";
 
 function About() {
   return (
-    <div className="text-gray-700 font-semibold justify-center content-center mt-36 flex">
-      <p>Under Development</p>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={2}
-        stroke="currentColor"
-        className="w-6 h-6 text-center ml-3"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
-        />
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-        />
-      </svg>
-    </div>
+    <>
+      <div className="w-full animate__animated animate__lightSpeedInLeft">
+        <div className=" px-2 sm:px-10 py-4">
+          <h2 className=" text-gray-700 sm:text-left md:pb-5 font-semibold">
+            About Us
+          </h2>
+        </div>
+        <div className="p-2 md:mx-10 rounded bg-slate-200">
+          <div className="md:p-4 text-center md:text-left md:w-10/12">
+            <p className="my-2 font-medium text-xs md:text-base">
+              A kenyan information technology company that specializes in
+              providing internet related services. We offer affordable web app
+              design,mobile application design and all other software
+              development services to organizations accross the globe. It's our
+              constant endeavor to continue being the leader in providing client
+              based customized web design, web development, customized software
+              development, SEO and solutions services
+            </p>
+          </div>
+        </div>
+      </div>
+      <Team />
+    </>
   );
 }
 
