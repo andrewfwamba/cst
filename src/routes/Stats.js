10584ffa07721221fde34f2893697418c99d0b6e
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-04-25 22:43:54 +0300

    restructure typo

--- src/routes/Stats.js
+++ src/routes/Stats.js
@@ -1,10 +1,14 @@
 import React from "react";
-import Marketing from "../components/Marketing";
 import Bargraph from "../utilities/Bargraph";
 import Footer from "../components/Footer";
 //
 
 function Stats() {
+  const stats = [
+    { id: 1, name: "Active Users", value: "500+" },
+    { id: 2, name: "Customer Satisfaction", value: "100%" },
+    { id: 3, name: "New users annually", value: "46,000" },
+  ];
   return (
     <>
       <div className="flex flex-col">
@@ -33,8 +37,24 @@ function Stats() {
             </div>
           </div>
         </div>
-        <div>
-          <Marketing />
+        <div className="bg-white py-4 sm:py-4 mt-2 sm:mt-2">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <dl className="grid grid-cols-1 gap-y-16 gap-x-8 text-center lg:grid-cols-3">
+              {stats.map((stat) => (
+                <div
+                  key={stat.id}
+                  className="mx-auto flex max-w-xs flex-col gap-y-4"
+                >
+                  <dt className="text-base leading-7 text-gray-600">
+                    {stat.name}
+                  </dt>
+                  <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                    {stat.value}
+                  </dd>
+                </div>
+              ))}
+            </dl>
+          </div>
         </div>
       </div>
       <Footer />
