853d26e82198db5a084f75fb1d8697a2881a5fe6
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-05-18 21:09:20 +0300

    terms of service

--- src/routes/Policy.js
+++ src/routes/Policy.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Refund from "../components/Refund";
 import Privacy from "../components/Privacy";
+import Terms from "../components/Terms";
 
 function Policy() {
   return (
@@ -8,6 +9,7 @@ function Policy() {
       <div className="text-lg bg-gray-300  text-gray-700 font-bold p-4 text-center md:text-xl md:text-left ">
         <h3 className="border-b border-gray-400 pb-2">Legal Terms</h3>
       </div>
+      <Terms />
       <Privacy />
       <Refund />
       <div className="w-full flex justify-between px-2 sm:px-10 py-2 text-xs text-gray-700 bg-gray-200">
