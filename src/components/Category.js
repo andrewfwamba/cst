dc89452f8bc7f78ca85cf48ce11a20addee18144
Andrew Fwamba <andrewecorrex03@gmail.com>
2024-05-15 22:05:54 +0300

    fix: project title upd

--- src/components/Category.js
+++ src/components/Category.js
@@ -39,7 +39,7 @@ export default function Category() {
     <div className="bg-gray-100">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-2xl py-4 sm:py-6 lg:max-w-none lg:py-8">
-          <h2 className="text-2xl font-bold text-gray-900">Collections</h2>
+          <h2 className="text-2xl font-bold text-gray-900">Projects</h2>
 
           <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
             {callouts.map((callout) => (
